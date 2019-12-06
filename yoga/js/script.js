9d8de1c41ecaa24ef9bf504e40883a663670cc87
Shakirov Andrey <46900900+AndrewShak@users.noreply.github.com>
2019-12-07 00:32:09 +0300

    пп
    В таймер добавлено кол-во дней, а также нули к целым числам. И если время закончиться, таймер обнуляется

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -36,32 +36,54 @@ window.addEventListener('DOMContentLoaded', function () {
 
     // Timer
 
-    let deadline = "2020-01-31"
+    let deadline = "2020-01-01";
 
-    function getTimeRemaning(endtume){
-        let t = Date.parse(endtume) - Date.parse(new Date()),       // получаем переменную оставшегося времени 
+    function getTimeRemaning(endtime){
+        let t = Date.parse(endtime) - Date.parse(new Date()),       // получаем переменную оставшегося времени 
         seconds = Math.floor((t / 1000) % 60),                  // Math.floor - получаем целлое округленное значение
         minutes = Math.floor((t / 1000/60) % 60 ),                      //при помощи % получаем секунды оставшиеся до минуты
-        hours = Math.floor((t/(1000*60*60)));
-        // hours = Math.floor((t/1000/60/60) % 24);         // При данном раскладе добавляется остатки дней
-        // days = Math.floor((t/(1000*60*60*24))
+        // hours = Math.floor((t/(1000*60*60)));
+        hours = Math.floor((t/1000/60/60) % 24),         // При данном раскладе добавляется остатки дней
+        days = Math.floor((t/(1000*60*60*24)));
 
         return {
             'total': t,
+            'days': days,
             'hours': hours,
             'minutes': minutes,
             'seconds': seconds
         };
     }
 
-    function setClock(id, endtume){
+    function setClock(id, endtime){
         let timer = document.getElementById(id),
+            days = timer.querySelector('.days'),
             hours = timer.querySelector('.hours'),
             minutes = timer.querySelector('.minutes'),
-            seconds = timer.querySelector('.seconds');
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
 
         function updateClock() {
-            let t = getTimeRemaning()
+            let t = getTimeRemaning(endtime);
+
+            function addZero(num) {
+                if (num <= 9) {
+                    return '0' + num;
+                } else return num;
+            };
+
+            days.textContent = addZero(t.days);
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                days.textContent = '00';
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
         }
     }
 
