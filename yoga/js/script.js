11ec26048e67f8442b9b00ebd42a3c732ca2da2a
Shakirov Andrey <46900900+AndrewShak@users.noreply.github.com>
2019-12-29 01:33:20 +0300

    Update script.js
    добавлены методы запросов JSON

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -1,30 +1,30 @@
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('DOMContentLoaded', function() {
 
     'use strict';
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
 
-    function hideTabContent (a) {                                               // Функция скрытия блока
-        for (let i = a; i < tabContent.length; i++) {                           // Запускаем цикл перебора блоков
-            tabContent[i].classList.remove('show');                      // Удаляем блок
-            tabContent[i].classList.add('hide');                                // добавляем блок
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
         }
     }
 
-    hideTabContent(1);                                                         // Запускаем функцию
+    hideTabContent(1);
 
-    function showTabContent(b) {                                                  // Функция показа блока
+    function showTabContent(b) {
         if (tabContent[b].classList.contains('hide')) {
             tabContent[b].classList.remove('hide');
             tabContent[b].classList.add('show');
         }
     }
 
-    info.addEventListener('click', function (event) {       //Функция показа или удаелния блока при нажатии на меню
+    info.addEventListener('click', function(event) {
         let target = event.target;
         if (target && target.classList.contains('info-header-tab')) {
-            for (let i = 0; i<tabContent.length; i++){
+            for(let i = 0; i < tab.length; i++) {
                 if (target == tab[i]) {
                     hideTabContent(0);
                     showTabContent(i);
@@ -32,79 +32,123 @@ window.addEventListener('DOMContentLoaded', function () {
                 }
             }
         }
+
     });
 
-    // Timer
+    // Timer 
 
-    let deadline = "2020-01-01";
+    let deadline = '2018-11-21';
 
-    function getTimeRemaning(endtime){
-        let t = Date.parse(endtime) - Date.parse(new Date()),       // получаем переменную оставшегося времени 
-        seconds = Math.floor((t / 1000) % 60),                  // Math.floor - получаем целлое округленное значение
-        minutes = Math.floor((t / 1000/60) % 60 ),                      //при помощи % получаем секунды оставшиеся до минуты
-        // hours = Math.floor((t/(1000*60*60)));
-        hours = Math.floor((t/1000/60/60) % 24),         // При данном раскладе добавляется остатки дней
-        days = Math.floor((t/(1000*60*60*24)));
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/(1000*60*60)));
 
         return {
-            'total': t,
-            'days': days,
-            'hours': hours,
-            'minutes': minutes,
-            'seconds': seconds
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
         };
     }
 
-    function setClock(id, endtime){
+    function setClock(id, endtime) {
         let timer = document.getElementById(id),
-            days = timer.querySelector('.days'),
             hours = timer.querySelector('.hours'),
             minutes = timer.querySelector('.minutes'),
             seconds = timer.querySelector('.seconds'),
             timeInterval = setInterval(updateClock, 1000);
-
+            
         function updateClock() {
-            let t = getTimeRemaning(endtime);
+            let t = getTimeRemaining(endtime);
 
-            function addZero(num) {
-                if (num <= 9) {
-                    return '0' + num;
-                } else return num;
-            };
+            function addZero(num){
+                        if(num <= 9) {
+                            return '0' + num;
+                        } else return num;
+                    };
 
-            days.textContent = addZero(t.days);
             hours.textContent = addZero(t.hours);
             minutes.textContent = addZero(t.minutes);
             seconds.textContent = addZero(t.seconds);
 
             if (t.total <= 0) {
                 clearInterval(timeInterval);
-                days.textContent = '00';
                 hours.textContent = '00';
                 minutes.textContent = '00';
                 seconds.textContent = '00';
             }
         }
-    }
 
+    }
 
     setClock('timer', deadline);
 
-    // MODAL
+    // Modal
 
     let more = document.querySelector('.more'),
         overlay = document.querySelector('.overlay'),
         close = document.querySelector('.popup-close');
 
-    more.addEventListener('click', function(){
+    more.addEventListener('click', function() {
         overlay.style.display = 'block';
-        this.classList.add('.more-splash');
-        document.body.style.overflow ='hidden';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
     });
 
-    close.addEventListener('click', function(){
+    close.addEventListener('click', function() {
         overlay.style.display = 'none';
-        more.classList.remove('.more-splash');
-        document.body.style.overflow ='';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
     });
+
+     // Form
+
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся!',
+        failure: 'Что-то пошло не так...'
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+        statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+        let formData = new FormData(form);
+
+        let obj = {};
+        formData.forEach(function(value, key) {
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        request.addEventListener('readystatechange', function() {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if(request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+    });
+
+
 });
