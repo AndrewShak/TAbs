31a34b53d5cd32c7bbc2b7388318602d9d95fb58
Shakirov Andrey <46900900+AndrewShak@users.noreply.github.com>
2020-04-28 01:52:36 +0300

    refresh

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -1,27 +1,28 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
 
     'use strict';
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
 
-    function hideTabContent(a) {
-        for (let i = a; i < tabContent.length; i++) {
-            tabContent[i].classList.remove('show');
-            tabContent[i].classList.add('hide');
+    function hideTabContent (a) {                                               // Функция скрытия блока
+        for (let i = a; i < tabContent.length; i++) {                           // Запускаем цикл перебора блоков
+            tabContent[i].classList.remove('show');                      // Удаляем блок
+            tabContent[i].classList.add('hide');                                // добавляем блок
         }
     }
 
-    hideTabContent(1);
 
-    function showTabContent(b) {
+    hideTabContent(1);                                              // Запускаем функцию
+
+    function showTabContent(b) {                                        // Функция показа блока
         if (tabContent[b].classList.contains('hide')) {
             tabContent[b].classList.remove('hide');
             tabContent[b].classList.add('show');
         }
     }
 
-    info.addEventListener('click', function(event) {
+    info.addEventListener('click', function(event) {            //Функция показа или удаелния блока при нажатии на меню
         let target = event.target;
         if (target && target.classList.contains('info-header-tab')) {
             for(let i = 0; i < tab.length; i++) {
@@ -35,15 +36,15 @@ window.addEventListener('DOMContentLoaded', function() {
 
     });
 
-    // Timer 
+    // Timer
 
-    let deadline = '2018-11-21';
+    let deadline = '2022-11-23';
 
     function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
-        seconds = Math.floor((t/1000) % 60),
-        minutes = Math.floor((t/1000/60) % 60),
-        hours = Math.floor((t/(1000*60*60)));
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
 
         return {
             'total' : t,
@@ -59,15 +60,15 @@ window.addEventListener('DOMContentLoaded', function() {
             minutes = timer.querySelector('.minutes'),
             seconds = timer.querySelector('.seconds'),
             timeInterval = setInterval(updateClock, 1000);
-            
+
         function updateClock() {
             let t = getTimeRemaining(endtime);
 
             function addZero(num){
-                        if(num <= 9) {
-                            return '0' + num;
-                        } else return num;
-                    };
+                if(num <= 9) {
+                    return '0' + num;
+                } else return num;
+            };
 
             hours.textContent = addZero(t.hours);
             minutes.textContent = addZero(t.minutes);
@@ -103,19 +104,19 @@ window.addEventListener('DOMContentLoaded', function() {
         document.body.style.overflow = '';
     });
 
-     // Form
+    // Form
 
     let message = {
-        loading: 'Загрузка...',
-        success: 'Спасибо! Скоро мы с вами свяжемся!',
-        failure: 'Что-то пошло не так...'
+        loading: 'Р—Р°РіСЂСѓР·РєР°...',
+        success: 'РЎРїР°СЃРёР±Рѕ! РЎРєРѕСЂРѕ РјС‹ СЃ РІР°РјРё СЃРІСЏР¶РµРјСЃСЏ!',
+        failure: 'Р§С‚Рѕ-С‚Рѕ РїРѕС€Р»Рѕ РЅРµ С‚Р°Рє...'
     };
 
     let form = document.querySelector('.main-form'),
         input = form.getElementsByTagName('input'),
         statusMessage = document.createElement('div');
 
-        statusMessage.classList.add('status');
+    statusMessage.classList.add('status');
 
     form.addEventListener('submit', function(event) {
         event.preventDefault();
@@ -150,5 +151,57 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    // Slider
+
+    let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+
+    showSlides(slideIndex);
+
+    function showSlides(n) {
+
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach((item) => item.style.display = 'none');
+        // for (let i = 0; i < slides.length; i++) {
+        //     slides[i].style.display = 'none';
+        // }
+        dots.forEach((item) => item.classList.remove('dot-active'));
+
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener('click', function() {
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', function() {
+        plusSlides(1);
+    });
+
+    dotsWrap.addEventListener('click', function(event) {
+        for (let i = 0; i < dots.length + 1; i++) {
+            if (event.target.classList.contains('dot') && event.target == dots[i-1]) {
+                currentSlide(i);
+            }
+        }
+    });
 
 });
