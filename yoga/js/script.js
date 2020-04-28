dac7b60c91e29037b792f88d8071c42f338c7ede
Shakirov Andrey <46900900+AndrewShak@users.noreply.github.com>
2020-04-28 15:04:42 +0300

    refresh

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -38,16 +38,18 @@ window.addEventListener('DOMContentLoaded', function () {
 
     // Timer
 
-    let deadline = '2020-11-23';
+    let deadline = '2020-05-15';
 
     function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
             seconds = Math.floor((t/1000) % 60),
             minutes = Math.floor((t/1000/60) % 60),
-            hours = Math.floor((t/(1000*60*60)));
+            hours = Math.floor((t/1000/60/60) % 24),
+            days = Math.floor((t/1000/60/60/24));
 
         return {
             'total' : t,
+            'days' : days,
             'hours' : hours,
             'minutes' : minutes,
             'seconds' : seconds
@@ -56,6 +58,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
     function setClock(id, endtime) {
         let timer = document.getElementById(id),
+            days = timer.querySelector('.days'),
             hours = timer.querySelector('.hours'),
             minutes = timer.querySelector('.minutes'),
             seconds = timer.querySelector('.seconds'),
@@ -70,12 +73,14 @@ window.addEventListener('DOMContentLoaded', function () {
                 } else return num;
             };
 
+            days.textContent = addZero(t.days);
             hours.textContent = addZero(t.hours);
             minutes.textContent = addZero(t.minutes);
             seconds.textContent = addZero(t.seconds);
 
             if (t.total <= 0) {
                 clearInterval(timeInterval);
+                days.textContent = '00';
                 hours.textContent = '00';
                 minutes.textContent = '00';
                 seconds.textContent = '00';
